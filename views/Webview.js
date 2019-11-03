371a269c060d7f08fc3b4ab0071cf0687868d09f
Sean Codes <sean_codes@outlook.com>
2019-11-03 14:33:38 -0600

    2019.4: emitter iframe date -> atom listener

--- views/Webview.js
+++ views/Webview.js
@@ -1,7 +1,7 @@
 module.exports = () => {
    return  `
       <div class="atombrowser-webview-container">
-         <webview id="atombrowser-webview" class="native-key-bindings" src="" />
+         <webview id="atombrowser-webview" preload="file:${__dirname}/../lib/atom-browser-emitter.js" class="native-key-bindings" src="" />
       </div>
    `.trim()
 }
